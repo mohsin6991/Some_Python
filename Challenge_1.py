9247c0ae7e00fc0b7ba199aa6ab23a16db2ca69c
mohsin6991 <mohesnm36@gmail.com>
2024-05-29 15:05:17 +0300

    first ch

---
+++ Challenge_1.py
@@ -0,0 +1,8 @@
+print("Challenge 1:") # print challange 1:
+message = "This is going to be tricky ;)"#create new variable as string 
+Message = "Very tricky!" #create new variable as String 
+print(message) #print first variable
+result = 3**2 #create new var as Arithmetic operators
+print("3**2 =", result) #print String With value of result 
+result = 3 - 5 #add new value in result 
+print("Challenge complete!") #print string 
