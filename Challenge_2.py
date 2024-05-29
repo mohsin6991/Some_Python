d9d2cbb26cb7391a9a4c5c1d3c1e94d0ccba69be
mohsin6991 <mohesnm36@gmail.com>
2024-05-29 15:06:34 +0300

    Add files via upload

---
+++ Challenge_2.py
@@ -0,0 +1,13 @@
+first_number=int(input('plese insert the first number '))
+second_number=int(input('plese insert the second number '))
+
+operation=input('enter the opreat you need to do plese ')
+
+if opriton!="+":
+    print("We dont suport this operation")
+elif opriton !="*":
+    print("We dont suport this operation")
+elif operation != "-":
+    print("We dont suport this operation")
+
+print('Thanks for using our softwaer')
---
+++ Challenge_3.py
@@ -0,0 +1,12 @@
+def calaulate(num1,num2,num3):
+    return (num1+num2)*num3
+print(calaulate(1,2,3))
+
+call_1=calaulate(10,20,30)
+print("Result of call 1 is:",call_1)
+
+call_2=calaulate(100,200,300)
+print("Result of call 1 is:",call_2)
+
+call_3=calaulate(5,10,15)
+print("Result of call 1 is:",call_3)
